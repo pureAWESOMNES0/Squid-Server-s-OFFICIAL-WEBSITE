c6f35c3c15c0ad54f9821b79d644772916423811
ComputingSquid <jwbolling001@gmail.com>
2020-07-08 15:06:39 +0000

    rename some folders, changed burttons

---
+++ functions.js
@@ -0,0 +1,30 @@
+;
+
+function notification() {
+  alert("To report abuse, Email jwbolling001@gmail.com and say, 'I report abuse'");
+}
+function reportDuplicate() {
+
+  var duplicateMaker = prompt("Who made the duplicate website? Please provide repl.it username or discord usename and tag");
+
+}
+
+
+function bloop() {
+  alert("BLOOOPPP! <br> BLOOPERS! 🦑🦑");
+}
+
+
+function verify() {
+  var areUSure = prompt("Are you sure you want to join the squid server?")
+
+
+  if(areUSure == 'YES') {
+    alert("Ok, the page shuld open up in a few seconds...");
+
+  }
+
+  else {
+    
+  }
+}
